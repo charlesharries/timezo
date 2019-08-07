52a636b1b72d0eef319690455bdc3b0cf6f9b835
Charles Harries <charlesgharries@gmail.com>
2019-08-07 20:16:21 +0100

    Rearrange db adapters file

--- db/adapters.js
+++ db/adapters.js
@@ -1,13 +1,14 @@
 import low from 'lowdb';
 import FileSync from 'lowdb/adapters/FileSync';
 
+/* User database */
 const usersAdapter = new FileSync('db/users.json');
 const usersDb = low(usersAdapter);
+usersDb.defaults({ users: [] }).write();
 
+/* Entries database */
 const entriesAdapter = new FileSync('db/entries.json');
 const entriesDb = low(entriesAdapter);
-
-usersDb.defaults({ users: [] }).write();
 entriesDb.defaults({ entries: [] }).write();
 
 export { usersDb, entriesDb };
