446c72e7d21acb540e490d45333d1c51abaca890
Charles Harries <charlesgharries@gmail.com>
2019-08-11 19:12:52 +0100

    Better error message handling on entries

--- components/Entries.js
+++ components/Entries.js
@@ -29,7 +29,7 @@ function Entries() {
 
       setState(oldState => ({ ...oldState, entries: newEntries }));
     } catch (err) {
-      console.log(err);
+      console.log(err.message);
     }
   }
 
