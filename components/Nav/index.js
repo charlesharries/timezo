6ad940d824a45ca1df0c64ce60f41fe4af6d1b86
Charles Harries <charlesgharries@gmail.com>
2019-08-11 19:52:54 +0100

    Add styling for Nav

---
+++ components/Nav/index.js
@@ -0,0 +1,14 @@
+import './style.css';
+
+function Nav() {
+  return (
+    <nav className="Nav">
+      <ul className="Nav__list">
+        <li className="Nav__item">Nav item</li>
+        <li className="Nav__item">Nav item</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
