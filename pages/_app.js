c32e9c18707ce64ba3d5372d416709121470ab6a
Charles Harries <charlesgharries@gmail.com>
2019-08-11 19:34:40 +0100

    Load user data on the server

--- pages/_app.js
+++ pages/_app.js
@@ -2,41 +2,44 @@ import App, { Container } from 'next/app';
 import axios from 'axios';
 import nookies from 'next-cookies';
 import { AppProvider } from '../store/app';
+import TheHeader from '../components/TheHeader';
 
 class CustomApp extends App {
-  static async getInitialProps({ ctx }) {
-    try {
-      const { token } = nookies(ctx);
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
 
-      const { data: user } = await axios({
-        method: 'get',
-        url: 'http://localhost:7777/api/users/me',
-        headers: {
-          Cookie: `token=${token}`,
-        },
-      });
-
-      if (user.error) {
-        return { user: null, err: user.error };
-      }
-
-      return { user };
-    } catch (err) {
-      console.log(err);
-      return { user: null, err };
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
     }
+
+    const { token } = nookies(ctx);
+
+    if (!token || !token.length) {
+      return { user: null, err: { message: 'no token' }, pageProps };
+    }
+
+    const { data } = await axios({
+      method: 'get',
+      url: 'http://localhost:7777/api/users/me',
+      headers: {
+        Cookie: `token=${token}`,
+      },
+    });
+
+    if (data.error) {
+      return { user: null, err: data, pageProps };
+    }
+
+    return { user: data, pageProps };
   }
 
   render() {
     const { Component, pageProps, user, err } = this.props;
 
-    if (err) {
-      console.log(err);
-    }
-
     return (
       <Container>
         <AppProvider user={user}>
+          <TheHeader />
           <Component {...pageProps} />
         </AppProvider>
       </Container>
