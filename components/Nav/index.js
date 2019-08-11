9250e4f6a27437d0255e6ab878d865657b7679bf
Charles Harries <charlesgharries@gmail.com>
2019-08-11 20:26:26 +0100

    Add structure for nav

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -1,11 +1,32 @@
+import Link from 'next/link';
+import { useContext } from 'react';
+import { AppContext } from '~store/app';
 import './style.css';
 
 function Nav() {
+  const [state] = useContext(AppContext);
+
+  const { user } = state;
+
   return (
     <nav className="Nav">
       <ul className="Nav__list">
-        <li className="Nav__item">Nav item</li>
-        <li className="Nav__item">Nav item</li>
+        <li className="Nav__item">
+          <Link href="/">
+            <a className="Nav__link link">Home</a>
+          </Link>
+        </li>
+        <li className="Nav__item">
+          {user ? (
+            <Link href="/signin">
+              <a className="Nav__link link">Sign in</a>
+            </Link>
+          ) : (
+            <Link href="/">
+              <a className="Nav__link link">Sign up</a>
+            </Link>
+          )}
+        </li>
       </ul>
     </nav>
   );
