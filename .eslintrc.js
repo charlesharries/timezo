7fca16814de6fd5c6d1b523d93682fb1382c7e4e
Charles Harries <charlesgharries@gmail.com>
2019-08-11 19:35:33 +0100

    Add build, eslint, postcss config

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+const { resolve } = require('path')
+
+module.exports = {
+  extends: [
+    "wesbos"
+  ],
+  rules: {
+    "jsx-a11y/label-has-for": [2, {
+      "required": {
+        "every": [ "id" ]
+      }
+    }],
+    "jsx-a11y/label-has-associated-control": [2, {
+      "required": {
+        "every": [ "id" ]
+      }
+    }],
+    "no-unused-expressions": ["error", {
+      "allowTernary": true
+    }],
+    "react/no-danger": "off",
+		"no-param-reassign": "off",
+		"react/self-closing-comp": "on"
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: {
+          resolve: {
+            alias: {
+              '~pages': resolve(__dirname, 'pages'),
+              '~components': resolve(__dirname, 'components'),
+              '~css': resolve(__dirname, 'assets', 'css'),
+              '~util': resolve(__dirname, 'util'),
+              '~config': resolve(__dirname, 'config')
+            },
+            extensions: ['.js', '.jsx', '.json']
+          }
+        }
+      }
+    },
+  },
+}
---
+++ next.config.js
@@ -0,0 +1,36 @@
+const withCSS = require('@zeit/next-css');
+require('dotenv').config();
+const { join, resolve } = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = withCSS({
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+        },
+      },
+    });
+
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '~pages': resolve(__dirname, 'pages'),
+      '~components': resolve(__dirname, 'components'),
+      '~css': resolve(__dirname, 'assets', 'css'),
+      '~util': resolve(__dirname, 'util'),
+      '~config': resolve(__dirname, 'config'),
+    };
+
+    config.plugins.push(
+      new Dotenv({
+        path: join(__dirname, '.env'),
+        systemvars: true,
+      })
+    );
+
+    return config;
+  },
+});
